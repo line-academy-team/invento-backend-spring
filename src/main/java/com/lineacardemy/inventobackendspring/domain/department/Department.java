a67aae34598a89a402515a4a951cee20f75b732f
ehdgus02 <ehdgus02kk@naver.com>
2026-08-24 14:36:00 +0900

    User Entity, Organization Entity, Department Entity 작성 완료

---
+++ src/main/java/com/lineacardemy/inventobackendspring/domain/department/Department.java
@@ -0,0 +1,40 @@
+package com.lineacardemy.inventobackendspring.domain.department;
+
+import com.lineacardemy.inventobackendspring.domain.common.BaseTimeEntity;
+import com.lineacardemy.inventobackendspring.domain.organization.Organization;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Department extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organization_id", nullable = false)
+    private Organization organization;
+
+    // TODO : Member 관계 작성
+
+    // TODO : Equipment 관계 작성
+
+    @Builder
+    public Department(String name, String description, Organization organization) {
+        this.name = name;
+        this.description = description;
+        this.organization = organization;
+    }
+}
--- src/test/java/com/lineacardemy/inventobackendspring/InventoBackendSpringApplicationTests.java
+++ src/test/java/com/lineacardemy/inventobackendspring/InventoBackendSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package com.lineacademy.inventobackendspring;
+package com.lineacardemy.inventobackendspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
