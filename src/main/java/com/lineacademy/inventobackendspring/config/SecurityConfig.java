2bb971d5795f2469765de856355de164968469c0
Tae.Garam (Windows) <garam@d3v-friends.com>
2026-08-27 23:27:55 +0900

    버그수정

--- src/main/java/com/lineacademy/inventobackendspring/config/SecurityConfig.java
+++ src/main/java/com/lineacademy/inventobackendspring/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.lineacademy.inventobackendspring.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,7 +23,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
